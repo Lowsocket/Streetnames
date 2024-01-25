b134ee4e1e3e9adfaac2217bcb60c099a91c2d03
Lowsocket <150552361+Lowsocket@users.noreply.github.com>
2024-01-25 14:20:08 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game "gta5"
+author "Kirill"
+discription "Streetnames and time and servername Made By Kirill"
+
+ui_page "ui/index.html"
+files {
+	"ui/index.html",
+	"ui/mic_click_on.ogg",
+	"ui/mic_click_off.ogg",
+	'ui/fonts/*.ttf',
+}
+shared_scripts {
+	"config.lua"
+}
+client_scripts {
+	"client.lua",
+}
